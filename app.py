d47975fba6c02cd780dfb821ff8aba5800c19414
Ritik Bompilwar <ritik19101@iiitnr.edu.in>
2021-08-22 19:21:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ class_names = ["COVID-19", "Normal", "Pneumonia"]
 
 @st.cache(suppress_st_warning=True)
 
-def get_data():
-    wget.download(link, 'test_set.zip')
-    with zipfile.ZipFile("test_set.zip","r") as zip_ref:
-        zip_ref.extractall("test_set")
-    return test_set
+#def get_data():
+wget.download(link, 'test_set.zip')
+with zipfile.ZipFile("test_set.zip","r") as zip_ref:
+    zip_ref.extractall("test_set")
+#    return test_set
 
 def predict(model_path):
     test_gen =ImageDataGenerator(featurewise_center=False, samplewise_center=False,
